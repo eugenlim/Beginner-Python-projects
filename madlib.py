8730ed87c7194a9d3f218e0ee7b2a109c924b318
eugenlim <eugenlim@users.noreply.github.com>
2020-12-12 16:02:04 +0800

    First Madlib exercise

---
+++ madlib.py
@@ -0,0 +1,20 @@
+# String concatenation (aka how to put strings together)
+# Suppose we want to create a string that says "Subscribe to__"
+youtuber = "Eugen Lim" # Some string variable
+
+# a few ways to do this
+print ("subscribe to " + youtuber)
+print ("subscribe to {} ".format(youtuber))
+print (f"subscribe to {youtuber}")
+
+
+adj = input ("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input ("Famous person: ")
+
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!  "
+
+print(madlib)
